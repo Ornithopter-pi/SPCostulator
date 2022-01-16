c55471fbc62f19ac99be1e7b3b16ae88505eca9f
Serissa Mironme <j.t.moore4700@gmail.com>
2022-01-16 11:54:58 -0500

    Changes to alert message

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const text = "Maybe it wasn't a mistake"
 
 
 
-window.alert(text)
+
+if (text.length < 16) { 
+    window.alert("no")
+}
+
+if (text.length > 16) {
+    window.alert("yes")
+}
+
+
+
+
+
+//  window.alert(text.length)
 
 
 
 // "Maybe it wasn't a mistake"
-// constant variable that 
+// constant variable that
+// output the number of characters in the string
+// alert yes if >16, alert no if <16 long
