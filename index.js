e19cf8e70030cc5d3e92c6d31033d5141d9f8dfc
Serissa Mironme <j.t.moore4700@gmail.com>
2022-01-17 03:04:25 -0500

    manipulating element properties

--- index.js
+++ index.js
@@ -3,13 +3,37 @@ const text = "Maybe it wasn't a mistake"
 
 
 
-if (text.length < 16) { 
-    window.alert("no")
-}
+const maincontent = document.getElementById("MainContent")
 
-if (text.length > 16) {
-    window.alert("yes")
-}
+
+
+window.alert(maincontent.innerText)
+
+
+
+
+maincontent.innerText="fish"
+
+
+
+
+window.alert(maincontent.innerText)
+
+
+
+
+
+
+
+
+
+// if (text.length < 16) { 
+//     window.alert("no")
+// }
+
+// if (text.length > 16) {
+//     window.alert("yes")
+// }
 
 
 
@@ -23,3 +47,5 @@ if (text.length > 16) {
 // constant variable that
 // output the number of characters in the string
 // alert yes if >16, alert no if <16 long
+// alert inner text value of Maincontent element
+// edit the text from javascript
